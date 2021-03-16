c59ee0f9419afe2a8f7ab3736474443dfbf55e8c
fashevskij <fashevskij@gmail.com>
2021-03-16 14:26:29 +0200

    add validation form (create post)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,7 @@ use App\Models\Rubric;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -33,6 +34,34 @@ class HomeController extends Controller
         dump($request->input('content'));
         dump($request->input('rubric_id'));*/
         //dd($request->all());
+
+        $this->validate($request,
+            [
+                'title' => ['required','min:5','max:25'],
+                'content'=> 'required',
+                'rubric_id'=> 'integer',
+
+            ]);
+
+        /*
+        $rules = [
+            'title' => ['required','min:5','max:25'],
+            'content'=> 'required',
+            'rubric_id'=> 'integer',
+        ];
+
+        $messages = [
+            'title.required' => 'заполните поле тайтла',
+            'title.min' => 'минимум 5 символов',
+            'title.max' => 'максимум 100 сиволов',
+
+            'rubric_id.integer' => 'выбирите рубрику',
+            'content.required'=> 'поле не должно быть пустым',
+
+        ];
+
+        $validator = Validator::make($request->all(),$rules, $messages)->validate();*/
+
         Post::query()->create($request->all());
         return redirect()->route('home');
     }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,19 +8,31 @@
     @parent
 @endsection
 @section('content')
+
+
+
     <div class="container mt-4">
+
+        @include('layouts.errors')
+
     <form method="post" action="{{ route('posts.store') }}">
         @csrf
         <div class="form-group mt-3">
             <label for="title">title</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
         </div>
         <div class="form-group mt-3">
             <label for="rubric_id">rubric</label>
-            <select class="form-control" id="rubric_id" name="rubric_id">
+            <select class="form-control" id="rubric_id" name="rubric_id" >
                 <option>select rubric</option>
                 @foreach($rubrics as $key => $value)
-                    <option value="{{$key}}">{{$value}}</option>
+                    <option value="{{$key}}"
+
+                            @if(old('rubric_id') == $key)
+                                selected
+                                @endif
+
+                    >{{$value}}</option>
                 @endforeach
 
             </select>
@@ -28,7 +40,7 @@
 
         <div class="form-group mt-3">
             <label for="content">content</label>
-            <textarea class="form-control" id="content" rows="3" name="content"></textarea>
+            <textarea class="form-control" id="content" rows="3" name="content" ></textarea>
         </div>
         <button type="submit" class="btn btn-dark mt-3">send</button>
     </form>
